604422269b525eb168171057e18db557e497c54d
Christoph Meyer <43418522+ChrisMayor@users.noreply.github.com>
2019-08-05 20:28:05 +0000

    Added Files

--- SSISPhoneLibShape/SSISPhoneLibDialog.Designer.cs
+++ SSISPhoneLibShape/SSISPhoneLibDialog.Designer.cs
@@ -28,19 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.chkInputColumns = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // chkInputColumns
+            // 
+            this.chkInputColumns.FormattingEnabled = true;
+            this.chkInputColumns.Location = new System.Drawing.Point(30, 52);
+            this.chkInputColumns.Name = "chkInputColumns";
+            this.chkInputColumns.Size = new System.Drawing.Size(268, 364);
+            this.chkInputColumns.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(459, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 444);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // SSISPhoneLibDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(548, 321);
+            this.ClientSize = new System.Drawing.Size(640, 498);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkInputColumns);
             this.Name = "SSISPhoneLibDialog";
             this.Text = "SSISPhoneLibDialog";
+            this.Load += new System.EventHandler(this.SSISPhoneLibDialog_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox chkInputColumns;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- SSISPhoneLibShape/SSISPhoneLibDialog.cs
+++ SSISPhoneLibShape/SSISPhoneLibDialog.cs
@@ -15,10 +15,46 @@ namespace SSISPhoneLibShape
     {
         public IDTSComponentMetaData100 ComponentMetaData { get; set; }
         public IServiceProvider ServiceProvider { get; set; }
+        private Dictionary<string, int> _columnInfo;
+        private bool _populatingList;
+        private IDTSInputColumn100 _column;
 
         public SSISPhoneLibDialog()
         {
             InitializeComponent();
+        }
+
+        private void SSISPhoneLibDialog_Load(object sender, EventArgs e)
+        {
+            
+            chkInputColumns.Visible = false;
+            chkInputColumns.Items.Clear();
+            IDTSVirtualInput100 virtualInput = ComponentMetaData.InputCollection[0].GetVirtualInput();
+
+
+            _columnInfo = new Dictionary<string, int>(virtualInput.VirtualInputColumnCollection.Count);
+
+            _populatingList = true;
+            foreach (IDTSVirtualInputColumn100 virtualColumn in virtualInput.VirtualInputColumnCollection)
+            {
+                int itemIndex = this.chkInputColumns.Items.Add(virtualColumn.Name);
+                chkInputColumns.SetItemChecked(itemIndex, virtualColumn.UsageType == DTSUsageType.UT_READWRITE);
+                _columnInfo.Add(virtualColumn.Name, virtualColumn.LineageID);
+            }
+
+            //txtNullValuesTable.Text =
+            //    ComponentMetaData.CustomPropertyCollection["NullDefaultTableName"].Value.ToString();
+
+            _populatingList = false;
+            chkInputColumns.Visible = true;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (_column != null)
+            {
+                //UpdateProperties(_column);
+            }
         }
     }
 }
